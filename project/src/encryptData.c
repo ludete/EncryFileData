1f2315cfbea0fae9396d70ea4a8471be36ba372e
ludete <yyxyong@163.com>
2017-05-04 23:17:15 +0800

    test OK certifice And public Key Two file Type

--- project/src/encryptData.c
+++ project/src/encryptData.c
@@ -7,23 +7,194 @@
 
 #include "encryptData.h"
 
-#define PRIVATEKEY 	"client-key.pem"
-#define PUBLICKEY 	"client-cert.pem"
 
+char* my_encrypt02(char *str, int lenth, char *publicPathKey)
+{
+	char *pEnData = NULL;
+	RSA  *pRsa = NULL;
+	FILE *fpKey = NULL;
+	int  rsaLen = 0;
+	
+	if(!str || lenth < 0 || !publicPathKey)
+	{
+		myprint("Err : str : %p, lenth : %d, publicPathKey : %p", str, lenth, publicPathKey);       
+        goto End;  
+	}
+	
+	//1. open the public Key
+	if((fpKey = fopen(publicPathKey,"r")) == NULL)
+	{
+		myprint("Err : func fopen() %s", publicPathKey);       
+        goto End;  
+    }   
+	
+	//2. get The news from public Key
+	//if((pRsa = PEM_read_RSA_PUBKEY(fpKey, NULL, NULL, NULL)) == NULL)	
+	if((pRsa = PEM_read_RSAPublicKey(fpKey, NULL, NULL, NULL)) == NULL)	
+	{
+		myprint("Err : func PEM_read_RSA_PUBKEY()");      
+		ERR_print_errors_fp(stdout); 
+        goto End;   
+	}
+	
+	//3. get The Lenth from  public Key News
+	rsaLen = RSA_size(pRsa);	
+	pEnData = (unsigned char *)malloc(rsaLen + 1);
+	memset(pEnData, 0, rsaLen + 1 );
+	
+	//4. public Key encrypt Data
+	if((RSA_public_encrypt(rsaLen, (unsigned char *)str, (unsigned char *)pEnData, pRsa, RSA_NO_PADDING)) < 0)
+	{
+		myprint("Err : func RSA_public_encrypt()");
+		goto End;        
+	}
+	
+	
+End:
+	
+	if(pRsa)  			RSA_free(pRsa);
+	if(fpKey)			fclose(fpKey);
+	
+	return (char *)pEnData;
+}
+
+
+char* my_decrypt02(char *str, int lenth, char *privatePathKey)
+{
+	char *pDeData = NULL;
+	RSA  *pRsa = NULL;
+	FILE *fpKey = NULL;
+	int  rsaLen = 0;
+	
+	if(!str || lenth < 0 || !privatePathKey)
+	{
+		myprint("Err : str : %p, lenth : %d, privatePathKey : %p", str, lenth, privatePathKey);       
+        goto End;  
+	}
+	
+	//1. open the private Key
+	if((fpKey = fopen(privatePathKey,"r")) == NULL)
+	{
+		myprint("Err : func fopen() %s", privatePathKey);       
+        goto End;  
+    }   
+    
+    //2. get The news from private Key
+    if((pRsa = PEM_read_RSAPrivateKey(fpKey, NULL, NULL, NULL)) == NULL)
+    {
+    	myprint("Err : func PEM_read_RSAPrivateKey() ");   
+    	ERR_print_errors_fp(stdout);    
+        goto End; 
+    }
+    
+    //3. get The Lenth from  private Key News
+	rsaLen = RSA_size(pRsa);	
+	pDeData = (unsigned char *)malloc(rsaLen + 1);
+	memset(pDeData, 0, rsaLen + 1 );
+    
+    if((RSA_private_decrypt(rsaLen,  (unsigned char *)str,  (unsigned char *)pDeData, pRsa, RSA_NO_PADDING)) < 0)
+    {
+    	myprint("Err : func RSA_private_decrypt()");
+		goto End; 
+    }
+    
+End:
+	
+	if(pRsa)  			RSA_free(pRsa);
+	if(fpKey)			fclose(fpKey);
+	
+	return (char *)pDeData;
+}
+
+
+//从证书中获取公钥信息
 char* my_encrypt(char *str, int lenth, char *publicPathKey)
 {
 	char *pEnData = NULL;
 	RSA  *pRsa = NULL;
 	FILE *fpKey = NULL;
+	int  rsaLen = 0;
+	BIO  *b = NULL; 
+	X509 *x = NULL;      
+    EVP_PKEY *k = NULL;  
+    
+	if(!str || lenth < 0 || !publicPathKey)
+	{
+		myprint("Err : str : %p, lenth : %d, publicPathKey : %p", str, lenth, publicPathKey);       
+        goto End;  
+	}
+	
+	b=BIO_new_file(publicPathKey,"rb"); 
+	x=PEM_read_bio_X509(b,NULL,NULL,NULL);  
+    k=X509_get_pubkey(x);  
+    pRsa=EVP_PKEY_get1_RSA(k); 
+	
+	//3. get The Lenth from  public Key News
+	rsaLen = RSA_size(pRsa);	
+	pEnData = (unsigned char *)malloc(rsaLen + 1);
+	memset(pEnData, 0, rsaLen + 1 );
+	
+	//4. public Key encrypt Data
+	if((RSA_public_encrypt(rsaLen, (unsigned char *)str, (unsigned char *)pEnData, pRsa, RSA_NO_PADDING)) < 0)
+	{
+		myprint("Err : func RSA_public_encrypt()");
+		goto End;        
+	}
 	
 	
+End:
 	
-	return NULL;
+	if(pRsa)  			RSA_free(pRsa);
+	if(fpKey)			fclose(fpKey);
+	
+	return (char *)pEnData;
 }
 
 
 char* my_decrypt(char *str, int lenth, char *privatePathKey)
 {
+	char *pDeData = NULL;
+	RSA  *pRsa = NULL;
+	FILE *fpKey = NULL;
+	int  rsaLen = 0;
 	
-	return NULL;
+	if(!str || lenth < 0 || !privatePathKey)
+	{
+		myprint("Err : str : %p, lenth : %d, privatePathKey : %p", str, lenth, privatePathKey);       
+        goto End;  
+	}
+	
+	//1. open the private Key
+	if((fpKey = fopen(privatePathKey,"r")) == NULL)
+	{
+		myprint("Err : func fopen() %s", privatePathKey);       
+        goto End;  
+    }   
+    
+    //2. get The news from private Key
+    if((pRsa = PEM_read_RSAPrivateKey(fpKey, NULL, NULL, NULL)) == NULL)
+    {
+    	myprint("Err : func PEM_read_RSAPrivateKey() ");   
+    	ERR_print_errors_fp(stdout);    
+        goto End; 
+    }
+    
+    //3. get The Lenth from  private Key News
+	rsaLen = RSA_size(pRsa);	
+	pDeData = (unsigned char *)malloc(rsaLen + 1);
+	memset(pDeData, 0, rsaLen + 1 );
+    
+    if((RSA_private_decrypt(rsaLen,  (unsigned char *)str,  (unsigned char *)pDeData, pRsa, RSA_NO_PADDING)) < 0)
+    {
+    	myprint("Err : func RSA_private_decrypt()");
+		goto End; 
+    }
+    
+End:
+	
+	if(pRsa)  			RSA_free(pRsa);
+	if(fpKey)			fclose(fpKey);
+	
+	return (char *)pDeData;
 }
+
--- project/src/test_encry_dec.c
+++ project/src/test_encry_dec.c
@@ -4,16 +4,37 @@
 
 #include "encryptData.h"
 
+#define PRIVATEKEY 	"/home/yyx/MyEncryProject/client2/client-key.pem"
+#define PUBLICKEY 	"/home/yyx/MyEncryProject/client2/client-cert.pem"
+
 int main()
 {
 	int ret = 0;
-	char* str = NULL;
+	char *str = "hello world";
+	char *encryData = NULL;
+	char *decData = NULL;
 	
-	my_encrypt(str, 0, NULL);
 	
-	my_decrypt(str, 0, NULL);
+	if((encryData = my_encrypt(str, strlen(str), PUBLICKEY)) == NULL)
+	{
+		ret = -1;
+		myprint("Err : func my_encrypt()");
+		goto End;
+	}
 	
-	myprint("hello world");
+	if((decData = my_decrypt(encryData, strlen(encryData), PRIVATEKEY)) == NULL)
+	{
+		ret = -1;
+		myprint("Err : func my_encrypt()");
+		goto End;
+	}
 	
+	if(strcmp(str, decData) == 0)		myprint("OK, encrypt : %s And decrypt : %s success",encryData, decData );
+	else								myprint("Err, encrypt : %s And decrypt : %s fail", encryData, decData);
+		
+	
+	myprint("====================== End ===================");
+
+End:
 	return ret;
 }

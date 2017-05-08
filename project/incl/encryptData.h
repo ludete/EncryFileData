5b5f031ab760403d7d7ac17a7c4053688dc69363
ludete <yyxyong@163.com>
2017-05-08 17:07:10 +0800

    encrypt File a half

--- project/incl/encryptData.h
+++ project/incl/encryptData.h
@@ -6,29 +6,41 @@
 extern "C" {
 #endif
 
-
+#define FILENAMELENTH  1024
 
 #define myprint( x...) do {char bufMessagePut_Stdout_[1024];\
         sprintf(bufMessagePut_Stdout_, x);\
         fprintf(stdout, "%s [%d], [%s]\n", bufMessagePut_Stdout_,__LINE__, __FILE__ );\
    }while (0)
 
-/*加密
+#define  MY_MIN(x, y)	((x) < (y) ? (x) : (y))
+
+/*加密; 采用公钥
 *@param : str 加密数据地址
 *@param : len 加密数据长度
 *@param : publicPathKey 指定加密数据公钥
 *@retval: success encrypt Data address; fail NULL;
 */
-char* my_encrypt(char *str, int lenth, char *publicPathKey);
+char* my_encrypt_publicKey(char *str, int lenth, char *publicPathKey);
 
 
-/*解密
+/*解密; 采用私钥;
 *@param : str 加密数据地址
 *@param : len 加密数据长度
 *@param : privatePathKey 指定解密数据私钥
 *@retval: success encrypt Data address; fail NULL;
 */
 char* my_decrypt(char *str, int lenth, char *privatePathKey);
+
+
+/*加密; 采用证书; 证书与公钥的不同之处在于: 证书中包含 公钥和 其他申请人信息
+*@param : str 加密数据地址
+*@param : len 加密数据长度
+*@param : publicPathKey 指定加密数据公钥
+*@retval: success encrypt Data address; fail NULL;
+*/
+char* my_encrypt_publicCert(char *str, int lenth, int *enDataLenth, char *publicPathKey);
+
 
 #ifdef __cplusplus
 }
--- project/src/encryptData.c
+++ project/src/encryptData.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<assert.h>
 #include<openssl/rsa.h>
 #include<openssl/pem.h>
 #include<openssl/err.h>
 
 #include "encryptData.h"
 
-
-char* my_encrypt02(char *str, int lenth, char *publicPathKey)
+//公钥加密信息
+char* my_encrypt_publicKey(char *str, int lenth, char *publicPathKey)
 {
 	char *pEnData = NULL;
 	RSA  *pRsa = NULL;
@@ -39,11 +40,12 @@ char* my_encrypt02(char *str, int lenth, char *publicPathKey)
 	
 	//3. get The Lenth from  public Key News
 	rsaLen = RSA_size(pRsa);	
-	pEnData = (unsigned char *)malloc(rsaLen + 1);
+	pEnData = malloc(rsaLen + 1);
 	memset(pEnData, 0, rsaLen + 1 );
 	
 	//4. public Key encrypt Data
-	if((RSA_public_encrypt(rsaLen, (unsigned char *)str, (unsigned char *)pEnData, pRsa, RSA_NO_PADDING)) < 0)
+	//if((RSA_public_encrypt(rsaLen, (unsigned char *)str, (unsigned char *)pEnData, pRsa, RSA_NO_PADDING)) < 0)
+	if((RSA_public_encrypt(lenth, (unsigned char *)str, (unsigned char *)pEnData, pRsa, RSA_PKCS1_PADDING)) < 0)
 	{
 		myprint("Err : func RSA_public_encrypt()");
 		goto End;        
@@ -55,99 +57,59 @@ End:
 	if(pRsa)  			RSA_free(pRsa);
 	if(fpKey)			fclose(fpKey);
 	
-	return (char *)pEnData;
+	return pEnData;
 }
 
 
-char* my_decrypt02(char *str, int lenth, char *privatePathKey)
-{
-	char *pDeData = NULL;
-	RSA  *pRsa = NULL;
-	FILE *fpKey = NULL;
-	int  rsaLen = 0;
-	
-	if(!str || lenth < 0 || !privatePathKey)
-	{
-		myprint("Err : str : %p, lenth : %d, privatePathKey : %p", str, lenth, privatePathKey);       
-        goto End;  
-	}
-	
-	//1. open the private Key
-	if((fpKey = fopen(privatePathKey,"r")) == NULL)
-	{
-		myprint("Err : func fopen() %s", privatePathKey);       
-        goto End;  
-    }   
-    
-    //2. get The news from private Key
-    if((pRsa = PEM_read_RSAPrivateKey(fpKey, NULL, NULL, NULL)) == NULL)
-    {
-    	myprint("Err : func PEM_read_RSAPrivateKey() ");   
-    	ERR_print_errors_fp(stdout);    
-        goto End; 
-    }
-    
-    //3. get The Lenth from  private Key News
-	rsaLen = RSA_size(pRsa);	
-	pDeData = (unsigned char *)malloc(rsaLen + 1);
-	memset(pDeData, 0, rsaLen + 1 );
-    
-    if((RSA_private_decrypt(rsaLen,  (unsigned char *)str,  (unsigned char *)pDeData, pRsa, RSA_NO_PADDING)) < 0)
-    {
-    	myprint("Err : func RSA_private_decrypt()");
-		goto End; 
-    }
-    
-End:
-	
-	if(pRsa)  			RSA_free(pRsa);
-	if(fpKey)			fclose(fpKey);
-	
-	return (char *)pDeData;
-}
-
-
-//浠庤瘉涔︿腑鑾峰彇鍏挜淇℃伅
-char* my_encrypt(char *str, int lenth, char *publicPathKey)
+//证书加密信息
+char* my_encrypt_publicCert(char *str, int lenth, int *enDataLenth, char *publicPathKey)
 {
 	char *pEnData = NULL;
 	RSA  *pRsa = NULL;
 	FILE *fpKey = NULL;
-	int  rsaLen = 0;
 	BIO  *b = NULL; 
 	X509 *x = NULL;      
     EVP_PKEY *k = NULL;  
     
-	if(!str || lenth < 0 || !publicPathKey)
+	if(!str || lenth < 0 || !publicPathKey || !enDataLenth)
 	{
 		myprint("Err : str : %p, lenth : %d, publicPathKey : %p", str, lenth, publicPathKey);       
         goto End;  
 	}
+	printf("=========== 11 =============\n");
+
+	//1.get The public Key from cert
+	if((b = BIO_new_file(publicPathKey,"rb")) == NULL)		assert(0);	 
+	if((x=PEM_read_bio_X509(b,NULL,NULL,NULL)) == NULL)		assert(0); 
+    if((k=X509_get_pubkey(x)) == NULL)						assert(0); 
+    if((pRsa=EVP_PKEY_get1_RSA(k)) == NULL)					assert(0);
 	
-	b=BIO_new_file(publicPathKey,"rb"); 
-	x=PEM_read_bio_X509(b,NULL,NULL,NULL);  
-    k=X509_get_pubkey(x);  
-    pRsa=EVP_PKEY_get1_RSA(k); 
-	
-	//3. get The Lenth from  public Key News
-	rsaLen = RSA_size(pRsa);	
-	pEnData = (unsigned char *)malloc(rsaLen + 1);
-	memset(pEnData, 0, rsaLen + 1 );
-	
-	//4. public Key encrypt Data
-	if((RSA_public_encrypt(rsaLen, (unsigned char *)str, (unsigned char *)pEnData, pRsa, RSA_NO_PADDING)) < 0)
+	 printf("=========== 12 =============\n");
+	 
+	//2. alloc The encrypData Content
+	pEnData = malloc(RSA_size(pRsa) + 1);
+	memset(pEnData, 0, RSA_size(pRsa) + 1 );
+
+	 printf("=========== 13  RSA_size(pRsa) : %d =============\n",  RSA_size(pRsa));
+
+	//3. public Key encrypt Data
+	if((*enDataLenth = RSA_public_encrypt(lenth, (unsigned char *)str, (unsigned char *)pEnData, pRsa, RSA_PKCS1_PADDING)) < 0)
 	{
 		myprint("Err : func RSA_public_encrypt()");
+		free(pEnData);
+		pEnData = NULL;
 		goto End;        
-	}
-	
+	} 
+	printf("=========== 14 =============\n");
 	
 End:
 	
 	if(pRsa)  			RSA_free(pRsa);
 	if(fpKey)			fclose(fpKey);
+	if(k)				EVP_PKEY_free(k);   
+    if(b)    			BIO_free(b);  
 	
-	return (char *)pEnData;
+	return pEnData;
 }
 
 
@@ -156,7 +118,6 @@ char* my_decrypt(char *str, int lenth, char *privatePathKey)
 	char *pDeData = NULL;
 	RSA  *pRsa = NULL;
 	FILE *fpKey = NULL;
-	int  rsaLen = 0;
 	
 	if(!str || lenth < 0 || !privatePathKey)
 	{
@@ -179,14 +140,16 @@ char* my_decrypt(char *str, int lenth, char *privatePathKey)
         goto End; 
     }
     
-    //3. get The Lenth from  private Key News
-	rsaLen = RSA_size(pRsa);	
-	pDeData = (unsigned char *)malloc(rsaLen + 1);
-	memset(pDeData, 0, rsaLen + 1 );
+    //3. get The Lenth from  private Key News	
+	pDeData = malloc(lenth + 1);
+	memset(pDeData, 0, lenth + 1 );
     
-    if((RSA_private_decrypt(rsaLen,  (unsigned char *)str,  (unsigned char *)pDeData, pRsa, RSA_NO_PADDING)) < 0)
+    //if((RSA_private_decrypt(rsaLen,  (unsigned char *)str,  (unsigned char *)pDeData, pRsa, RSA_NO_PADDING)) < 0)
+	if((RSA_private_decrypt(lenth, (unsigned char *)str, (unsigned char *)pDeData, pRsa, RSA_PKCS1_PADDING)) < 0)
     {
     	myprint("Err : func RSA_private_decrypt()");
+		free(pDeData);
+		pDeData = NULL;
 		goto End; 
     }
     
@@ -195,6 +158,52 @@ End:
 	if(pRsa)  			RSA_free(pRsa);
 	if(fpKey)			fclose(fpKey);
 	
-	return (char *)pDeData;
+	return pDeData;
+}
+
+
+int encryptFileData(char *filePath)
+{
+	int ret = 0;
+	FILE *srcFp = NULL, *encryFp = NULL;
+	char *tmp = NULL;
+	char decFileName[FILENAMELENTH];
+	char srcContent[245];
+
+	if(!filePath)
+	{
+		myprint("Err : filePath : %p", filePath); 	  
+		goto End;  
+	}
+
+	//1. open The file
+	if((srcFp = fopen(filePath, "rb")) == NULL)		assert(0);
+
+	//2. get The SrcFile suffix
+	memset(decFileName, 0, FILENAMELENTH);
+	if((tmp = strrchr(filePath, '.')))		sprintf(decFileName, "%s", );		
+	{
+		memcpy(decFileName, filePath, strlen(filePath) - strlen(tmp));
+		strcat(decFileName, "_ENCRYPT");
+		strcat(decFileName, tmp);
+	}
+	else
+	{
+		memcpy(decFileName, filePath, strlen(filePath) - strlen(tmp));
+		strcat(decFileName, "_ENCRYPT");
+	}
+
+	//3. create The encrypt File
+	if((encryFp = fopen(decFileName, "ab+")) == NULL)		assert(0);
+
+	//2. encrypt Data from file; once 245 BYTE
+	while(!(feof(fp)))
+	{
+		
+	}
+			
+End:
+
+	return ret;
 }
 
